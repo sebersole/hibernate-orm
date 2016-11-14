c5993d6cd571835e19e1e5e0be731e0e07b1318f
Steve Ebersole <steve@hibernate.org>
2016-11-14 11:54:09 -0600

    HHH-10954 - Integrate SQM into Hibernate ORM (ProcedureCall/SPQ); HHH-10530 - Add support for calling SQL functions and retrieving the function result

--- hibernate-core/src/main/java/org/hibernate/persister/common/spi/SingularAttributeImplementor.java
+++ hibernate-core/src/main/java/org/hibernate/persister/common/spi/SingularAttributeImplementor.java
@@ -6,14 +6,13 @@
  */
 package org.hibernate.persister.common.spi;
 
-import org.hibernate.sqm.domain.SingularAttribute;
+import org.hibernate.sqm.domain.SingularAttributeReference;
 import org.hibernate.type.mapper.spi.Type;
 
 /**
  * @author Steve Ebersole
  */
-public interface SingularAttributeImplementor extends SingularAttribute {
-	@Override
+public interface SingularAttributeImplementor extends SingularAttributeReference, SqmTypeImplementor {
 	Type getType();
 	Column[] getColumns();
 }
