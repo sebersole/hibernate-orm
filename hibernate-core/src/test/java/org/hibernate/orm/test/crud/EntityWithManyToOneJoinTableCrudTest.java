6dd418f11f7b553d5395fb26eb2861855fcbfd81
Andrea Boriero <andrea@hibernate.org>
2018-06-07 12:16:40 +0100

    Add test for ManyToOne with JoinTable

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/crud/EntityWithManyToOneJoinTableCrudTest.java
@@ -0,0 +1,85 @@
+package org.hibernate.orm.test.crud;
+
+import java.time.Instant;
+import java.util.Calendar;
+
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.orm.test.SessionFactoryBasedFunctionalTest;
+import org.hibernate.orm.test.support.domains.gambit.EntityWithManyToOneJoinTable;
+import org.hibernate.orm.test.support.domains.gambit.SimpleEntity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * @author Andrea Boriero
+ */
+public class EntityWithManyToOneJoinTableCrudTest extends SessionFactoryBasedFunctionalTest {
+
+	@Override
+	protected void applyMetadataSources(MetadataSources metadataSources) {
+		super.applyMetadataSources( metadataSources );
+		metadataSources.addAnnotatedClass( EntityWithManyToOneJoinTable.class );
+		metadataSources.addAnnotatedClass( SimpleEntity.class );
+	}
+
+	@Override
+	protected boolean exportSchema() {
+		return true;
+	}
+
+	@Test
+	public void testOperations() {
+		EntityWithManyToOneJoinTable entity = new EntityWithManyToOneJoinTable( 1, "first", Integer.MAX_VALUE );
+
+		SimpleEntity other = new SimpleEntity(
+				2,
+				Calendar.getInstance().getTime(),
+				null,
+				Integer.MAX_VALUE,
+				Long.MAX_VALUE,
+				null
+		);
+
+		entity.setOther( other );
+
+		sessionFactoryScope().inTransaction( session -> session.save( other ) );
+		sessionFactoryScope().inTransaction( session -> session.save( entity ) );
+
+		sessionFactoryScope().inTransaction(
+				session -> {
+					final EntityWithManyToOneJoinTable loaded = session.get( EntityWithManyToOneJoinTable.class, 1 );
+					assert loaded != null;
+					assertThat( loaded.getName(), equalTo( "first" ) );
+					assert loaded.getOther() != null;
+					assertThat( loaded.getOther().getId(), equalTo( 2 ) );
+				}
+		);
+
+		sessionFactoryScope().inTransaction(
+				session -> {
+					final SimpleEntity loaded = session.get( SimpleEntity.class, 2 );
+					assert loaded != null;
+					assertThat( loaded.getSomeInteger(), equalTo( Integer.MAX_VALUE ) );
+				}
+		);
+
+
+		sessionFactoryScope().inTransaction(
+				session -> {
+					final String value = session.createQuery(
+							"select e.name from EntityWithManyToOneJoinTable e where e.other.id = 2",
+							String.class
+					).uniqueResult();
+					assertThat( value, equalTo( "first" ) );
+				}
+		);
+
+
+	}
+
+
+}
+
