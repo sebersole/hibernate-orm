ef0ea4d20c7a36462b7b2ae890edc0e9b4f2c0d2
Andrea Boriero <andrea@hibernate.org>
2018-07-23 10:23:21 +0100

    Add OneToMany test

---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/crud/EntityWithOneToManyCrudTest.java
@@ -0,0 +1,66 @@
+package org.hibernate.orm.test.crud;
+
+import java.util.Calendar;
+
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.orm.test.SessionFactoryBasedFunctionalTest;
+import org.hibernate.orm.test.support.domains.gambit.EntityWithOneToMany;
+import org.hibernate.orm.test.support.domains.gambit.SimpleEntity;
+
+import org.hibernate.testing.junit5.FailureExpected;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+/**
+ * @author Andrea Boriero
+ */
+@FailureExpected( value= "Persist and find og OneToMany has not yet been implemented" )
+public class EntityWithOneToManyCrudTest extends SessionFactoryBasedFunctionalTest {
+	@Override
+	protected void applyMetadataSources(MetadataSources metadataSources) {
+		super.applyMetadataSources( metadataSources );
+		metadataSources.addAnnotatedClass( EntityWithOneToMany.class );
+		metadataSources.addAnnotatedClass( SimpleEntity.class );
+	}
+
+	@Override
+	protected boolean exportSchema() {
+		return true;
+	}
+
+	@Test
+	public void testSave() {
+		EntityWithOneToMany entity = new EntityWithOneToMany( 1, "first", Integer.MAX_VALUE );
+
+		SimpleEntity other = new SimpleEntity(
+				2,
+				Calendar.getInstance().getTime(),
+				null,
+				Integer.MAX_VALUE,
+				Long.MAX_VALUE,
+				null
+		);
+
+		entity.addOther( other );
+
+		sessionFactoryScope().inTransaction(
+				session -> {
+					session.save( other );
+				} );
+
+		sessionFactoryScope().inTransaction(
+				session -> {
+					session.save( entity );
+				} );
+
+		sessionFactoryScope().inTransaction(
+				session -> {
+					EntityWithOneToMany retrieved = session.get( EntityWithOneToMany.class, 1 );
+					assertThat( retrieved, notNullValue() );
+				} );
+
+	}
+}
+
---
+++ hibernate-core/src/test/java/org/hibernate/orm/test/support/domains/gambit/EntityWithOneToMany.java
@@ -0,0 +1,65 @@
+package org.hibernate.orm.test.support.domains.gambit;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ * @author Andrea Boriero
+ */
+@Entity
+public class EntityWithOneToMany {
+	private Integer id;
+
+	// alphabetical
+	private String name;
+	private List<SimpleEntity> others = new ArrayList<>(  );
+	private Integer someInteger;
+
+	public EntityWithOneToMany(Integer id, String name, Integer someInteger) {
+		this.id = id;
+		this.name = name;
+		this.someInteger = someInteger;
+	}
+
+	@Id
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@OneToMany
+	public List<SimpleEntity> getOthers() {
+		return others;
+	}
+
+	public void setOthers(List<SimpleEntity> others) {
+		this.others = others;
+	}
+
+	public Integer getSomeInteger() {
+		return someInteger;
+	}
+
+	public void setSomeInteger(Integer someInteger) {
+		this.someInteger = someInteger;
+	}
+
+	public void addOther(SimpleEntity other) {
+		others.add( other );
+	}
+}
+
