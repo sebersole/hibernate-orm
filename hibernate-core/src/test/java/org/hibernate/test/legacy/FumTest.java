da18b8d7e4af6d67d8a6f8f7df353eff43464dba
Andrea Boriero <andrea@hibernate.org>
2019-07-11 18:23:39 +0100

    6 - SQM based on JPA type system

--- hibernate-core/src/test/java/org/hibernate/test/legacy/FumTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/legacy/FumTest.java
@@ -35,7 +35,7 @@ import org.hibernate.FlushMode;
 import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.LockMode;
-import org.hibernate.Query;
+import org.hibernate.query.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
