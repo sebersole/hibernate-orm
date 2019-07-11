8749910770eac0e48a0b5c50008b98655334af71
Andrea Boriero <andrea@hibernate.org>
2019-07-11 11:40:12 +0100

    6 - SQM based on JPA type system

--- hibernate-core/src/main/java/org/hibernate/engine/internal/StatefulPersistenceContext.java
+++ hibernate-core/src/main/java/org/hibernate/engine/internal/StatefulPersistenceContext.java
@@ -892,7 +892,7 @@ public class StatefulPersistenceContext implements PersistenceContext {
 	}
 
 	@Override
-	public CollectionEntry addInitializedCollection(CollectionPersister persister, PersistentCollection collection, Object id)
+	public CollectionEntry addInitializedCollection(CollectionPersister persister, PersistentCollection collection, Serializable id)
 			throws HibernateException {
 		final CollectionEntry ce = new CollectionEntry( collection, persister, id, flushing );
 		ce.postInitialize( collection );
