44be98b7de860314e74544816463a62dc360cfb7
Andrea Boriero <andrea@hibernate.org>
2019-08-19 10:11:28 +0100

    6 - SQM based on JPA type system

--- hibernate-core/src/main/java/org/hibernate/persister/entity/AbstractEntityPersister.java
+++ hibernate-core/src/main/java/org/hibernate/persister/entity/AbstractEntityPersister.java
@@ -163,6 +163,7 @@ import org.hibernate.type.EntityType;
 import org.hibernate.type.Type;
 import org.hibernate.type.TypeHelper;
 import org.hibernate.type.VersionType;
+import org.hibernate.type.descriptor.java.JavaTypeDescriptor;
 
 /**
  * Basic functionality for persisting an entity via JDBC
@@ -1865,6 +1866,11 @@ public abstract class AbstractEntityPersister
 				.setOuterJoins( "", "" )
 				.setWhereClause( whereClause )
 				.toStatementString();
+	}
+
+	@Override
+	public JavaTypeDescriptor getExpressableJavaTypeDescriptor() {
+		return getEntityKeyDefinition().getExpressableJavaTypeDescriptor();
 	}
 
 	protected interface InclusionChecker {
